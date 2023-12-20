a205dd34b290ae529d034b24e87313c8720134e8
Nishanth-N-G <nishanthsomu64@gmail.com>
2023-12-20 18:23:40 +0530

    Business logic update for negative test cases

--- src/main/java/com/candidate/Election/Service/CandidateService.java
+++ src/main/java/com/candidate/Election/Service/CandidateService.java
@@ -4,6 +4,7 @@ import com.candidate.Election.Model.Candidate;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,8 +14,13 @@ public class CandidateService {
     List<Candidate> electionCandidates=new ArrayList<>();
 
     public synchronized String registerCandidate(String name) {
-        electionCandidates.add(new Candidate( name, 0));
-        return "You are Registered for Election. All the best.";
+        if(name.isEmpty()) return "You need to enter your name for Registration.";
+        for(Candidate candidate: electionCandidates){
+            if(candidate.getName().equals(name))
+                return name+" you are already registered.";
+        }
+        electionCandidates.add(new Candidate(name, 0));
+        return name+" you are Registered for Election. All the best.";
     }
 
     public synchronized String castMyVote(String name) {
@@ -25,7 +31,7 @@ public class CandidateService {
         if(candidate.isPresent()) {
             Candidate foundCandidate = candidate.get();
             foundCandidate.setVotes(foundCandidate.getVotes()+1);
-            return "You have voted for your Candidate.";
+            return "You have voted for "+foundCandidate.getName();
         }else{
             return "Sorry! We didn't find your Candidate.";
         }
@@ -38,8 +44,7 @@ public class CandidateService {
 
         if(candidate.isPresent()) {
             Candidate foundCandidate = candidate.get();
-            int votes= foundCandidate.getVotes();
-            return "You have " +votes+ " votes.";
+            return  foundCandidate.getName()+" have " +foundCandidate.getVotes()+ " votes.";
         }else{
             return "Sorry! We didn't find your Candidate to check votes.";
         }
@@ -50,14 +55,27 @@ public class CandidateService {
     }
 
     public synchronized String announceWinner() {
-        Optional<Candidate> winner = electionCandidates.stream().max((c1, c2) -> Integer.compare(c1.getVotes(), c2.getVotes()));
+        Optional<Candidate> winner = electionCandidates.stream().max(Comparator.comparingInt(Candidate::getVotes));
         if(winner.isPresent()) {
             Candidate win = winner.get();
-            if(win.getVotes()==0){
-                return "No Winner yet.";
-            }else {
-                return "Winner of this election is " + win.getName() + " with " + win.getVotes() + " votes!! Congratulation.";
+            int maxVotes = win.getVotes();
+            if(maxVotes==0){
+                return "All Candidates are with 0 votes, No Winner yet.";
             }
+            List<String> tieResult=new ArrayList<>();
+            for(Candidate candidate: electionCandidates) {
+                if (candidate.getVotes() == maxVotes) {
+                    tieResult.add(candidate.getName());
+                }
+            }
+            if(tieResult.size()>1){
+                String tieNamesList="";
+                for(String name:tieResult){
+                    tieNamesList= tieNamesList.concat(name+" ");
+                }
+                return "The Election is tied between "+tieNamesList;
+            }
+            return "Winner of this election is " + win.getName() + " with " + win.getVotes() + " votes!! Congratulation.";
         }else{
             return "No Winner yet.";
         }
