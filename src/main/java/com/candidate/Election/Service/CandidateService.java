14a1119caadfd6608b88f8af74f20db1a16b0de6
Nishanth-N-G <nishanthsomu64@gmail.com>
2023-12-20 08:31:36 +0530

    Addition of Completed Controller class

--- src/main/java/com/candidate/Election/Service/CandidateService.java
+++ src/main/java/com/candidate/Election/Service/CandidateService.java
@@ -1,21 +1,64 @@
 package com.candidate.Election.Service;
 
+import com.candidate.Election.Model.Candidate;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class CandidateService {
 
+    List<Candidate> electionCandidates=new ArrayList<>();
+
     public String registerCandidate(String name) {
+        electionCandidates.add(new Candidate( name, 0));
+        return "You are Registered for Election";
     }
 
     public String castMyVote(String name) {
+        Optional<Candidate> candidate = electionCandidates.stream()
+                .filter(c -> c.getName().equals(name))
+                .findFirst();
+
+        if(candidate.isPresent()) {
+            Candidate foundCandidate = candidate.get();
+            foundCandidate.setVotes(foundCandidate.getVotes()+1);
+            return "You have voted for your Candidate";
+        }else{
+            return "Sorry! We didn't find your Candidate";
+        }
     }
 
     public String countCandidateVote(String name) {
+        Optional<Candidate> candidate = electionCandidates.stream()
+                .filter(c -> c.getName().equals(name))
+                .findFirst();
+
+        if(candidate.isPresent()) {
+            Candidate foundCandidate = candidate.get();
+            int votes= foundCandidate.getVotes();
+            return "You have " +votes+ " votes";
+        }else{
+            return "Sorry! We didn't find your Candidate to check votes";
+        }
     }
 
     public List<Candidate> countAllVotes() {
+        return electionCandidates;
     }
 
     public String announceWinner() {
+        Optional<Candidate> winner = electionCandidates.stream().max((c1, c2) -> Integer.compare(c1.getVotes(), c2.getVotes()));
+        if(winner.isPresent()) {
+            Candidate win = winner.get();
+            if(win.getVotes()==0){
+                return "No Winner yet";
+            }else {
+                return "Winner of this election is " + win.getName() + " with " + win.getVotes() + " votes!! Congratulation";
+            }
+        }else{
+            return "No Winner yet";
+        }
+
     }
 }
