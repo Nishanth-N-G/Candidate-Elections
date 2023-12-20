b798ce13c48a1c8e558fead89faedc88fa73f94b
Nishanth-N-G <nishanthsomu64@gmail.com>
2023-12-20 08:03:07 +0530

    Addition of Completed Controller class

---
+++ src/main/java/com/candidate/Election/Controller/CandidateController.java
@@ -0,0 +1,46 @@
+package com.candidate.Election.Controller;
+
+import com.candidate.Election.Model.Candidate;
+import com.candidate.Election.Service.CandidateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CandidateController {
+
+    @Autowired
+    private CandidateService candidateService;
+
+    @PostMapping("/registerCandidate")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String registerCandidate(@RequestParam String name){
+        return candidateService.registerCandidate(name);
+    }
+
+    @PutMapping("/castVote")
+    @ResponseStatus(HttpStatus.OK)
+    public String castMyVote(@RequestParam String name){
+        return candidateService.castMyVote(name);
+    }
+
+    @GetMapping("/countCandidateVote")
+    @ResponseStatus(HttpStatus.OK)
+    public String countCandidateVote(@RequestParam String name){
+        return candidateService.countCandidateVote(name);
+    }
+
+    @GetMapping("/listVotes")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Candidate> countAllVotes(){
+        return candidateService.countAllVotes();
+    }
+
+    @GetMapping("/getWinner")
+    @ResponseStatus(HttpStatus.OK)
+    public String announceWinner(){
+        return candidateService.announceWinner();
+    }
+}
---
+++ src/main/java/com/candidate/Election/Service/CandidateService.java
@@ -0,0 +1,21 @@
+package com.candidate.Election.Service;
+
+import java.util.List;
+
+public class CandidateService {
+
+    public String registerCandidate(String name) {
+    }
+
+    public String castMyVote(String name) {
+    }
+
+    public String countCandidateVote(String name) {
+    }
+
+    public List<Candidate> countAllVotes() {
+    }
+
+    public String announceWinner() {
+    }
+}
